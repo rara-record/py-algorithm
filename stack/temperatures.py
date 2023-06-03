15c460f0d7c75b785e1bd5cfd52ee06a5149c86f
BORA <a52447879@gmail.com>
2023-06-03 17:47:46 +0900

    stack

---
+++ stack/temperatures.py
@@ -0,0 +1,14 @@
+
+def dailyTemperatures(temperatures):
+    ans = [0] * len(temperatures)
+    stack = []
+
+    for cur_day, _ in enumerate(temperatures):
+        while stack and temperatures[stack[-1]] < temperatures[cur_day]:
+            last_day = stack.pop()
+            ans[last_day] = cur_day - last_day
+        stack.append(cur_day)
+    return ans
+
+
+print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
---
+++ stack/temperatures.ts
@@ -0,0 +1,17 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+  const stack: number[] = [];
+  const ans: number[] = [];
+
+  // 역순으로 순회
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    while (
+      stack.length &&
+      temperatures[stack[stack.length - 1]] <= temperatures[i]
+    )
+      stack.pop();
+    ans.push(stack.length ? stack[stack.length - 1] - i : 0);
+    stack.push(i);
+  }
+
+  return ans.reverse();
+}
