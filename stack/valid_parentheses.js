c448fc567e9149b5ee3f4f1fc42c56c11b260019
BORA <a52447879@gmail.com>
2024-03-24 20:35:51 +0900

    fix(stack): valid_parentheses.js

--- stack/valid_parentheses.js
+++ stack/valid_parentheses.js
@@ -2,10 +2,18 @@ function solution(s) {
   const stack = [];
   let answers = ture;
   s.split("").forEach((element) => {
-    if (element === "(") stack.push(")");
-    if (element === "{") stack.push("}");
-    if (element === "[") stack.push("]");
-    if (stack.length && stack[stack.length - 1] === element) stack.pop();
+    if (element === "(") {
+      stack.push(")");
+    } else if (element === "{") {
+      stack.push("}");
+    } else if (element === "[") {
+      stack.push("]");
+    } else if (stack.length && stack[stack.length - 1] === element) {
+      stack.pop();
+    } else {
+      answers = false;
+      return;
+    }
   });
   if (stack.length) answers = false;
   return answers;
