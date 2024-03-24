824b290604b3264874686718c44ffbc2c4cf4a60
BORA <a52447879@gmail.com>
2024-03-24 20:26:26 +0900

    stack: temperatures.py

--- stack/temperatures.py
+++ stack/temperatures.py
@@ -1,13 +1,14 @@
-
 def daily_temperatures(temperatures):
-    ans = [0] * len(temperatures)
+    ans = [0] * len(temperatures) # answer 배열 초기화 
     stack = []
-
-    for cur_day, _ in enumerate(temperatures):
-        while stack and temperatures[stack[-1]] < temperatures[cur_day]:
-            last_day = stack.pop()
-            ans[last_day] = cur_day - last_day
-        stack.append(cur_day)
+    # O(n)
+    for cur_day, cur_temp in enumerate(temperatures): # enumerate(temperatures): index, temperatures 
+        # cur_temp보다 온도가 낮을 날이 있으면 pop을 해준다. 
+        # stack[-1] : 스택의 마지막요소, stack[1] 튜플 자료구조의 1번째, 즉 마지막 요소의 온도를 확인
+        while stack and stack[-1][1] < cur_temp: 
+            prev_day, _ = stack.pop()
+            ans[prev_day] = cur_day - prev_day
+        stack.append((cur_day, cur_temp))  
     return ans
 
 
